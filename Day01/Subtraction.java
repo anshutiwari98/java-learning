97760e5e3ef0ed880f3d52b1739f6ad3bbaa028f
Anshu Tiwari <anshutiwari6924@gmail.com>
2026-07-26 21:03:22 +0530

    This is Sub Program

---
+++ Day01/Subtraction.java
@@ -0,0 +1,13 @@
+public class Subtraction {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+        int sub = num1 - num2;
+
+        System.out.println(num1 + " - " + num2 + " = " + sub);
+        System.out.println("diff is " + sub);
+
+    }
+}

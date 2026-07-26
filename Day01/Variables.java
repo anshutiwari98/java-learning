f634478b75363b3009d8a6f4b2489582f5456082
Anshu Tiwari <anshutiwari6924@gmail.com>
2026-07-26 21:22:43 +0530

    Complete Day01 Java basics and variables

---
+++ Day01/Variables.java
@@ -0,0 +1,20 @@
+public class Variables{
+    public static void main(String[] args){
+
+        String name = "Ansu";
+        int age = 26;
+        double salary = 10.76;
+        char grade = 'A';
+        boolean isWorking = true;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age is : "+ age);
+        System.out.println("Salary is : "+ salary);
+        System.out.println("Grade is : "+ grade);
+        System.out.println("Is Anshu working ? : "+ isWorking);
+        
+
+
+
+    }
+}

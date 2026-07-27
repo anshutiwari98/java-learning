8259ffbd9d90851c99ef2f9b5e6e00bc4e855a48
Anshu Tiwari <anshutiwari6924@gmail.com>
2026-07-27 14:51:02 +0530

    Add Day03 data types programs

---
+++ Day03/EmployeeDetails.java
@@ -0,0 +1,23 @@
+public class EmployeeDetails{
+    public static void main(String[] args){
+        byte age = 26;
+        short experience = 3;
+        int employeeID = 101;
+        long mobileNumber =9876543210L;
+        float performanceScore = 4.5f;
+        double salary =85000.75;
+        char grade = 'A';
+        boolean isPermanentEmployee =true;
+
+        System.out.println("------------------------------------------------");
+        System.out.println("Employee ID        : " + employeeID);
+        System.out.println("Age                : " + age);
+        System.out.println("Experience         : "  + experience);
+        System.out.println("Mobile Number      : " + mobileNumber);
+        System.out.println("Performance Score  : " + performanceScore);
+        System.out.println("Salary             : " + salary);
+        System.out.println("Grade              : " + grade);
+        System.out.println("Permanent Employee : " + isPermanentEmployee);
+        System.out.println("------------------------------------------------");
+    }
+}

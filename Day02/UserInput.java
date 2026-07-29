2d35abe6401e45eebbad07819f2bd635dc5aeb0a
Anshu Tiwari <anshutiwari6924@gmail.com>
2026-07-29 12:43:56 +0530

    Complete Day02 Scanner and user input programs

--- Day02/UserInput.java
+++ Day02/UserInput.java
@@ -4,8 +4,8 @@ public class UserInput{
     public static void main(String[] args){
         
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name");
         String name = sc.nextLine();
+        System.out.println("Enter your name");
         System.out.println("My name is : " + name);
         
     
